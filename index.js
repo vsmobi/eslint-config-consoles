72db94224272085e64302cfaef42907a26af79e0
Vadim Cherepnin <vcherepnin@dev.vtb>
2021-03-01 16:49:13 +0300

    use airbnb

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 module.exports = {
-    extends: [
-        './base.js'
+    "extends": [
+        "airbnb-typescript"
     ],
-    parser: '@typescript-eslint/parser',
-    rules: {
-        'import/extensions': 'off',
-        'react/prop-types': 'off',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars-experimental': 'error'
+    "parserOptions": {
+        "project": "./tsconfig.json"
     },
-    plugins: [
-        '@typescript-eslint'
+    "plugins": [
+        "@typescript-eslint"
     ],
-    parserOptions: {
-        project: './tsconfig.json'
+    "rules": {
+        "@typescript-eslint/comma-dangle": [
+            "error",
+            "never"
+        ],
+        "@typescript-eslint/indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "@typescript-eslint/no-shadow": "off",
+        "@typescript-eslint/no-use-before-define": [
+            "error",
+            {
+                "functions": false
+            }
+        ],
+        "import/no-default-export": "error",
+        "import/no-unresolved": "off",
+        "import/prefer-default-export": "off",
+        "linebreak-style": "off",
+        "max-len": [
+            "error",
+            {
+                "code": 140,
+                "tabWidth": 4
+            }
+        ],
+        "object-curly-newline": "off",
+        "react/destructuring-assignment": "off",
+        "react/jsx-filename-extension": "off",
+        "react/jsx-indent": [
+            "error",
+            4
+        ],
+        "react/jsx-indent-props": [
+            "error",
+            4
+        ],
+        "react/jsx-props-no-spreading": "off",
+        "react/jsx-tag-spacing": "off",
+        "react/require-default-props": "off",
+        "react/forbid-prop-types": "off",
+        "react/prop-types": "off"
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true
     }
-};
+}
+;
